2475a4baf30ab5734a482839811499aa90b3c8ce
João Medeiros <jpmdrsdev@gmail.com>
2025-04-25 23:47:30 -0300

    style: add logo for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <Image
+        src="/Furia_Esports_logo.png"
+        alt="Logo da FURIA"
+        width={140}
+        height={140}
+        className="shadow-lg"
+      />
       <h1 className="text-4xl font-bold">Bem-vindo ao FURIA Chatbot</h1>
       <Link
         href="/chat"
